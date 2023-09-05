f80f5705e13cda3e9f8091c44c1e8363c73372b9
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2023-09-06 01:22:18 +0500

    object completed

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
 "use strict";
+let numbers = [2, 3, 3, 4, 6, 5, 45];
+var wave = numbers.map((data) => {
+    if (data < 18) {
+        return 2003;
+    }
+    else {
+        return 2023;
+    }
+});
+let num = [2, 5, 4, 3, 7, 6, 9, 7];
+// console.log(wave);
+let wave2 = numbers.map((data2, number) => {
+    if (data2 % 2 == 0) {
+        return "even";
+    }
+    else {
+        return number;
+    }
+});
+// console.log(wave2);
+var num3 = [4, 5, 6, 7, 89, 9];
+var wave3 = num3.map((index) => {
+    if (index == 89) {
+        return 150;
+    }
+    else {
+        return index;
+    }
+});
+// console.log(wave3);
+let beyon = [45, 67, 89, 34, 56, 778, 46];
+let varArray = beyon.filter((value) => {
+    return value % 2 == 0;
+});
+//console.log(varArray);
+let num6 = ["sarab", "wasif", "cheema"];
+var sndingmail = num6.forEach((names) => {
+    // console.log('hello jnb');
+});
+let num9 = [65, 78, 34, 56, 12, 34, 67];
+var newNum = num9.filter((sum) => {
+    return sum * 2 == 130;
+});
+console.log(newNum);
+var array = [34, 56, 78, 23, 32, 12];
+var updatedArray = array.map((add) => {
+    if (add > 45) {
+        return true;
+    }
+    else if (add > 30) {
+        return false;
+    }
+    else {
+        return 12;
+    }
+});
+var onlineArray = array.filter((sub) => {
+    if (sub < 45) {
+        return true;
+    }
+});
+console.log(updatedArray);
+console.log(onlineArray);
+//object start from here
 var userNames = ["sarab", "wasif", "ali", "akram"];
 var names = userNames.forEach((data) => {
     console.log(data);
@@ -12,7 +76,26 @@ let car = {
     model: "v 8",
     brand: "toyato",
     modelyear: 2023,
-    colou: "car",
+    colour: "car",
     inuse: false
 };
 console.log(car);
+var haveNames = ["ali", "haider", "waqas"];
+var newId = [];
+for (let i = 0; i < haveNames.length - 1; i++) {
+    newId.push(haveNames[i]);
+}
+console.log(newId);
+var student = {
+    class: "fsc",
+    rollno: 32,
+    section: "hero",
+    pass: true
+};
+var student2 = {
+    class: "fsc",
+    rollno: 34,
+    section: "hero",
+    pass: true
+};
+console.log(student.rollno, student2.rollno);
--- index.ts
+++ index.ts
@@ -1,3 +1,71 @@
+let numbers:number[] = [2,3,3,4,6,5,45];
+var wave= numbers.map((data)=>{
+    if(data<18){
+        return 2003;
+    }
+    else{
+        return 2023;
+    }
+})
+let num:number[] = [2,5,4,3,7,6,9,7];
+// console.log(wave);
+let wave2= numbers.map((data2,number)=>{
+    if(data2%2==0){
+        return "even";
+    }
+    else{
+        return number;
+    }
+})
+// console.log(wave2);
+var num3:number[] = [4,5,6,7,89,9];
+var wave3 = num3.map((index)=>{
+    if(index == 89){
+        return 150;
+    }
+    else{
+        return index;
+    }
+})
+// console.log(wave3);
+let beyon:number[] = [45,67,89,34,56,778,46];
+let varArray = beyon.filter((value)=>{
+return value%2 ==0;
+})
+ //console.log(varArray);
+let num6:string[] = ["sarab","wasif","cheema"];
+var sndingmail = num6.forEach((names)=>{
+   // console.log('hello jnb');
+    
+})
+
+let num9:number[] = [65,78,34,56,12,34,67];
+var newNum:any[] = num9.filter((sum)=>{
+    return sum * 2==130;
+})
+console.log(newNum);
+
+var array:number[]= [34,56,78,23,32,12];
+var updatedArray = array.map((add)=>{
+    if(add>45){
+        return true;
+    }
+    else if(add >30){
+       return false;
+    }
+    else{
+        return 12;
+    }
+})
+var onlineArray = array.filter((sub)=>{
+    if(sub<45){
+        return true
+    }
+})
+console.log(updatedArray);
+console.log(onlineArray);
+
+//object start from here
 var userNames:string[] = ["sarab","wasif","ali","akram"];
 var names = userNames.forEach((data)=>{
 console.log(data);
@@ -17,5 +85,30 @@ let car ={
 
 }
 console.log(car);
+
+var haveNames:string[] = ["ali","haider","waqas"];
+var newId:string[] = [];
+for(let i=0;i<haveNames.length-1;i++ ){
+newId.push(haveNames[i])
+}
+console.log(newId);
+
+type Typer = {class:string,section:string,rollno:number,marks:number,pass:boolean};
+var student: Typer = {
+    class: "fsc",
+    rollno: 32,
+    section: "hero",
+    pass: true,
+    marks: 121
+}
+var student2:Typer = {
+    class: "fsc",
+    rollno: 34,
+    section: "hero",
+    pass: true,
+    marks: 123
+}
+console.log(student.rollno,student2.rollno);
+
 
 
